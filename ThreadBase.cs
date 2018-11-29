043a495ee8bcf4e25ebdb62039a3ed6249fd7cc9
nineleaf <ncningchong@163.com>
2018-11-29 18:08:31 +0800

    Add files via upload

---
+++ ThreadBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Class_Ning
+{
+    public class ThreadBase
+    {
+
+        bool Flag = false;
+        Thread mythread;
+        private readonly object Mylock = new object();
+
+        public void Start()
+        {
+            lock(Mylock)
+            {
+                if (Flag != false)
+                {
+                    Flag = true;
+                    mythread = new Thread(new ThreadStart(Run));
+                    mythread.Start();
+                }
+            }
+            
+        }
+        public void Stop()
+        {
+            lock(Mylock)
+            {
+                Flag = false;
+            }
+        }
+        protected virtual void Run()
+        {
+           
+        }
+
+    }
+}
